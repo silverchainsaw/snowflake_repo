5d61501bd1445a3bc8f285659c30d65ba36a80b8
silverchainsaw <73755222+silverchainsaw@users.noreply.github.com>
2020-11-25 15:26:17 +0530

    Add files via upload
    2

---
+++ V1.2__Scripts_Next_Round.sql
@@ -0,0 +1,18 @@
+--Use this schema
+USE SCHEMA OUR_TEST_DB.OUR_TEST_SCHEMA;
+
+--Create Table
+CREATE TABLE OUR_TEST_DB.OUR_TEST_SCHEMA.CUSTOMER_NEW (
+  Name STRING ,
+  Email STRING ,
+  Job STRING ,
+  Phone STRING ,
+  Age NUMBER
+  );
+
+insert into OUR_TEST_DB.OUR_TEST_SCHEMA.CUSTOMER values('Robert','ab233c@hotmail.com','DE2','86000965',32);
+insert into OUR_TEST_DB.OUR_TEST_SCHEMA.CUSTOMER values('Ravish','qwerabc@hotmail.com','Admin','8900065',33);
+
+Update OUR_TEST_DB.OUR_TEST_SCHEMA.CUSTOMER
+set Name='James Bond'
+where Name='Javed';
